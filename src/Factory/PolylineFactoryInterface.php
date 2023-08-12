149afc8833d852eed2dfcb6fb9c49216c54e2d1e
n-marisov <116942294+n-marisov@users.noreply.github.com>
2023-08-12 19:51:12 +0300

    Добавлен PolylineFactoryInterface.

---
+++ src/Factory/PolylineFactoryInterface.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Maris\Interfaces\Geo\Factory;
+
+use Maris\Interfaces\Geo\Model\PolylineInterface;
+use stdClass;
+
+interface PolylineFactoryInterface
+{
+    /**
+     * Создает новую полилинию.
+     * @param iterable $coordinates
+     * @return PolylineInterface
+     */
+    public function new( iterable $coordinates ) : PolylineInterface;
+
+    /**
+     * Получает полилинию из строки.
+     * @param string $string
+     * @return PolylineInterface|null
+     */
+    public function fromString( string $string ) : ?PolylineInterface;
+
+    /***
+     * Получает полилинию из GeoJson.
+     * @param string|array|stdClass $coordinatesOrGeometry
+     * @return PolylineInterface|null
+     */
+    public function fromJson( string|array|stdClass $coordinatesOrGeometry ) : ?PolylineInterface;
+}
