16df29f308b0c97a751970f8c8db0aa1459340b2
n-marisov <116942294+n-marisov@users.noreply.github.com>
2023-08-13 11:06:22 +0300

    Добавлены методы для PolylineInterface.

--- src/Model/PolylineInterface.php
+++ src/Model/PolylineInterface.php
@@ -2,7 +2,9 @@
 
 namespace Maris\Interfaces\Geo\Model;
 
+use Countable;
 use IteratorAggregate;
+use Maris\Interfaces\Geo\Calculator\DistanceCalculatorInterface;
 use Traversable;
 
 /**
@@ -11,9 +13,86 @@ use Traversable;
  * @template TValue as LocationInterface
  * @template-implements Traversable<TKey, TValue>
  */
-interface PolylineInterface extends GeometryInterface, IteratorAggregate
+interface PolylineInterface extends GeometryInterface, IteratorAggregate, Countable
 {
     /**
+     * Возвращает первую точку полилинии
+     * @return LocationInterface|LocationAggregateInterface|null
+     */
+    public function getStart():LocationInterface|LocationAggregateInterface|null;
+
+    /**
+     * Возвращает последнюю точку полилинии
+     * @return LocationInterface|LocationAggregateInterface|null
+     */
+    public function getEnd():LocationInterface|LocationAggregateInterface|null;
+
+    /**
+     * Добавляет точку в конец полилинии.
+     * @param LocationInterface|LocationAggregateInterface $location
+     * @return $this
+     */
+    public function add( LocationInterface|LocationAggregateInterface $location ):static;
+
+    /***
+     * Определяет наличие точки в полилинии.
+     * @param LocationInterface|LocationAggregateInterface $location
+     * @return bool
+     */
+    public function contains( LocationInterface|LocationAggregateInterface $location ):bool;
+    //public function has( LocationInterface|LocationAggregateInterface $location ):bool;
+    /**
+     * Получает точку по позиции в списке.
+     * @param positive-int $position
+     * @return LocationInterface|LocationAggregateInterface|null
+     */
+    public function get( int $position ):LocationInterface|LocationAggregateInterface|null;
+
+    /**
+     * Удаляет точку по позиции или объекту.
+     * @param int|LocationInterface|LocationAggregateInterface $locationOrPosition
+     * @return LocationInterface|LocationAggregateInterface|null
+     */
+    public function remove( int|LocationInterface|LocationAggregateInterface $locationOrPosition ):LocationInterface|LocationAggregateInterface|null;
+
+    /**
+     * Добавляет точку только если она отсутствует в полилинии.
+     * Возвращает bool была ли добавлена точка.
+     * @param LocationInterface|LocationAggregateInterface $location
+     * @return bool
+     */
+    public function addUnique( LocationInterface|LocationAggregateInterface $location ):bool;
+
+    /***
+     * Определяет, является ли полилиния
+     * отрезком из двух точек.
+     * @return bool
+     */
+    public function isSection():bool;
+
+    /**
+     * Возвращает лист из содержащий в себе только
+     * полилинии которые являются отрезками ( isSection() === true )
+     * созданные из текущей полилинии.
+     * @return array<PolylineInterface>
+     */
+    public function getSections():array;
+
+    /**
+     * Получает протяженность линии.
+     * @param DistanceCalculatorInterface $calculator
+     * @return float
+     */
+    public function getLength( DistanceCalculatorInterface $calculator ):float;
+
+    /***
+     * Возвращает полилинию развернутую в обратную сторону.
+     * @return PolylineInterface
+     */
+    public function getReverse():PolylineInterface;
+
+    /**
+     * Возвращает итератор для переборки всех точек полилинии.
      * @return Traversable<LocationInterface>
      */
     public function getIterator(): Traversable;
